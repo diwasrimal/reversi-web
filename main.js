8e25c39b7d020e550fd2c60beb00b6b62a82a4ec
Diwas10 <rimaldiwas10@gmail.com>
2023-08-28 18:13:08 +0545

    Add code to handle game over

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ class Game {
   rows = 8;
   cols = this.rows;
   colors = ['red', 'green'];
+  validMoves = [];
   firstTurn = this.colors[0];
+
 
   constructor() {
     const boxes = Array.from(document.querySelectorAll('.board button'));
@@ -20,14 +22,16 @@ class Game {
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
         const button = this.board[i][j];
-        button.addEventListener('click', () => {
-          if (this.validMove(i, j))
+        const moveIfValid = () => {
+          if (this.validMoves.includes(button)) {
             this.makeMove(i, j);
-        })
+            button.removeEventListener('click', moveIfValid);
+          }
+        }
+        button.addEventListener('click', moveIfValid);
       }
     }
-
-    this.updateRound();
+    this.updateStates();
   }
 
   isEmpty(i, j) {
@@ -39,14 +43,14 @@ class Game {
     return (i < 0 || j < 0 || i > this.rows - 1 || j > this.cols - 1);
   }
 
-  validMove(i, j) {
+  isValidMove(i, j) {
     const supportingColor = this.colors[this.round % 2];
 
-    // Invalid if box is filled
+    // Invalid if piece already taken
     if (!this.isEmpty(i, j))
       return false;
 
-    // Loop through neighboring points
+    // Loop thorough neighboring points to check validtity of move
     for (let x = i - 1; x <= i + 1; x++) {
       for (let y = j - 1; y <= j + 1; y++) {
 
@@ -72,29 +76,27 @@ class Game {
         let a = x + movementVector.x;
         let b = y + movementVector.y;
         while (!this.outOfBounds(a, b) && !this.isEmpty(a, b)) {
-          if (this.board[a][b].className == supportingColor)
+          if (this.board[a][b].className === supportingColor)
             return true;
           a += movementVector.x;
           b += movementVector.y;
         }
       }
     }
-    return false;
+
   }
 
   makeMove(i, j) {
-    const color = this.colors[this.round % 2];
+    const supportingColor = this.colors[this.round % 2];
 
-    // Place the move at given position
-    this.board[i][j].className = color;
-
-    // Loop through neighboring points
-    // and find consumable pieces
+    // Opponent's pieces that this move will consume
     let consumables = [];
+
+    // Loop thorough neighboring points to check validtity of move
     for (let x = i - 1; x <= i + 1; x++) {
       for (let y = j - 1; y <= j + 1; y++) {
 
-        // Skip pieces not in board array
+        // Skip pieces not in board
         if (this.outOfBounds(x, y))
           continue;
 
@@ -102,6 +104,13 @@ class Game {
         if (this.isEmpty(x, y))
           continue;
 
+        // Piece adjacent to moving place cannot be same
+        if (this.board[x][y].className == supportingColor)
+          continue;
+
+        // Check if supporting piece with same color is on other side
+        // if yes this move will be valid
+        // Check along the line stretched by (i, j) and neighbor (x, y)
         const movementVector = {
           x: x - i,
           y: y - j
@@ -111,22 +120,29 @@ class Game {
         let pieces = [];
 
         while (!this.outOfBounds(a, b) && !this.isEmpty(a, b)) {
-          if (this.board[a][b].className == color) {
+          const piece = this.board[a][b];
+          if (piece.className === supportingColor) {
             consumables.push(...pieces);
             break;
           }
-          pieces.push(this.board[a][b]);
+          pieces.push(piece)
           a += movementVector.x;
           b += movementVector.y;
         }
       }
     }
 
-    for (const consumable of consumables) {
-      consumable.className = color;
-    }
+    console.log("conumes: ", consumables);
 
-    this.updateRound();
+    if (consumables.length === 0)
+      throw new Error("No pieces could be consumed!");
+
+
+    consumables.push(this.board[i][j]);
+    for (const consumable of consumables)
+      consumable.className = supportingColor;
+
+    this.updateStates();
   }
 
   colorCount(color) {
@@ -140,14 +156,50 @@ class Game {
     return count;
   }
 
-  updateRound() {
-    this.round++;
-    let newTurn = (this.round % 2 == 0) ? 'Red' : 'Green';
-    document.querySelector('#turn').innerHTML = newTurn;
-    document.querySelector('#red-count').innerHTML = this.colorCount('red');
-    document.querySelector('#green-count').innerHTML = this.colorCount('green');
+  showGameResult() {
+    let winner;
+    const reds = this.colorCount('red');
+    const greens = this.colorCount('green');
+    if (reds > greens) winner = 'Red';
+    if (greens > reds) winner = 'Green';
+
+    const result = document.createElement('div');
+    if (!winner)
+      result.innerHTML = "TIE"
+    else
+      result.innerHTML = `Winner: <span class="${winner.toLowerCase()}">${winner}</span>`
+
+    const turn = document.querySelector("#game-details").children[1];
+    turn.replaceWith(result);
+
+    document.querySelector('#game-result').classList.remove('hidden');
   }
 
+  updateStates() {
+    this.round++;
+    this.validMoves = [];
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        if (this.isValidMove(i, j))
+          this.validMoves.push(this.board[i][j])
+      }
+    }
+
+    if (this.validMoves.length === 0) {
+      this.showGameResult();
+      return;
+    }
+
+    const newTurn = (this.round % 2 == 0) ? 'Red' : 'Green';
+    const turnElement = document.querySelector('#turn');
+    turnElement.innerHTML = newTurn;
+    turnElement.className = newTurn.toLowerCase();
+    document.querySelector('#red-count').innerHTML = this.colorCount('red');
+    document.querySelector('#green-count').innerHTML = this.colorCount('green');
+
+  }
 }
+
+
 
 const game = new Game();
